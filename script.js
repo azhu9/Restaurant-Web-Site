7518a87eb1b5b58413a37e927ffa2f14c9b8c807
azhu9 <109678994+azhu9@users.noreply.github.com>
2023-01-05 22:02:23 -0500

    Contact Fields
    contact fields work ish

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const button = document.querySelector("button");
-function cateringAndReservations () {
-  
-}
+// const button = document.querySelector("button");
+const submitButton = document.querySelector("#submit-button");
+// function cateringAndReservations () {
+// }
+// document.getElementById("last-modified").innerHTML = "This page was last updated: "+document.lastModified;
 
-document.getElementById("last-modified").innerHTML = "This page was last updated: "+document.lastModified;  
+submitButton.onclick = function(){
+    document.getElementById("resume-input").value = "";
+    document.getElementById("first-input").value = "";
+    document.getElementById("last-input").value = "";
+    document.getElementById("email-input").value = "";
+}
