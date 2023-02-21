29eb2d0f7db0af4cee2b5148dba273ec2174393e
Andy Zhu <109678994+azhu9@users.noreply.github.com>
2023-02-21 19:09:01 +0000

    Presenting Platform Shift

--- script.js
+++ script.js
@@ -154,3 +154,31 @@ submitButton.onclick = function(){
     startConfetti();
     setTimeout(stopConfetti, 2500);
 }
+var secret = "3838404037393739666513"; //Konami Code
+var input = "";
+var timer;
+var mode = false;
+var showclassName =false;
+$(document).ready(function() {
+$('#flash').hide(); });
+
+
+
+$(document).keyup(function(e) {
+//alert(e.which);
+input += e.which;
+clearTimeout(timer);
+timer = setTimeout(function() { input = ""; }, 500);
+check_input(); });
+
+
+
+function check_input() {
+if(input == secret) {
+//the secret code
+showclassName= true; }};
+
+
+
+$(document).ready(function() {
+setInterval(function() { $('#info').html('Keystroke: ' + input); }, 100); });
